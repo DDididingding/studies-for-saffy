6ba0f4aa8a029fca018d093ddc1ffd827cf57ffd
junhyun <erik1201@naver.com>
2023-06-30 22:44:26 +0900

    day 1

---
+++ first.py
@@ -0,0 +1,33 @@
+# r = 800
+# t = 110
+# size = r*t/1000
+
+# print("size: ", size, "MB")
+
+#----------------------------------------------------
+
+# family = ["mom", "dad", "sister", "brother"]
+
+# family.remove("sister")
+
+# print(family)
+
+#----------------------------------------------------
+
+# print('직각삼각형 그리기\n')
+# leg = int(input('변의 길이: '))
+
+# for i in range(leg):
+#     print('* ' * (i + 1))
+
+# area = (leg ** 2) / 2
+# print('넓이:', area)
+
+#----------------------------------------------------
+
+#제곱수 출력
+a = int(input())
+
+print(a*a)
+
+
